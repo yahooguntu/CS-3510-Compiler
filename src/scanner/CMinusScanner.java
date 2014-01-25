5ff9202b7e27478e65bcdc08d275ac3515387ff6
Seth Yost <Seth Yost@163.11.26.242>
2014-01-25 14:19:13 -0500

    Implemeted the C- Scanner Class

--- src/scanner/CMinusScanner.java
+++ src/scanner/CMinusScanner.java
@@ -1,6 +1,34 @@
 package scanner;
 
-public class CMinusScanner 
+import java.io.BufferedReader;
+
+public class CMinusScanner implements Scanner
 {
-	//comment
+	private BufferedReader inFile;
+	private Token nextToken;
+	
+	public CMinusScanner (BufferedReader file)
+	{
+		inFile = file;
+		nextToken = scanToken();
+	}
+	
+	public Token getNextToken()
+	{
+		Token returnToken = nextToken;
+		if(nextToken.getType() != Token.TokenType.EOF_TOKEN)
+		{
+			nextToken = scanToken();
+		}
+		return returnToken;
+	}
+	
+	public Token viewNextToken(){
+		return nextToken;
+	}
+	
+	public Token scanToken()
+	{
+		return new Token();
+	}
 }
