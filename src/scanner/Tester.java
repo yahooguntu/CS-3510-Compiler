c611fcb23f66b2b3392b522e05042bbb978e4aab
Seth Yost <Seth Yost@163.11.169.248>
2014-02-04 21:40:02 -0500

    writeoutput method

--- src/scanner/Tester.java
+++ src/scanner/Tester.java
@@ -1,38 +1,46 @@
 package scanner;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 import scanner.Token.TokenType;
 
 public class Tester
 {
-	private BufferedReader outFile;
+	private static BufferedWriter outFile;
 	
 	public static void main(String[] args)
 	{
 		Scanner s = new CMinusScanner(new File("TestFile.cm"));
-		
-		Token t = s.getNextToken();
-		while (t.getType() != TokenType.EOF)
-		{
-			System.out.println(t.getType().toString() + "\t:" + t.getData());
-			t = s.getNextToken();
-		}
-	}
-	
-	private void writeToken()
-	{
-		/*
 		try {
-			//outFile.write(viewNextToken().toString());
-			outFile.write("");
+			outFile = new BufferedWriter(new FileWriter("output.txt"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
+		Token t = s.getNextToken();
+		while (t.getType() != TokenType.EOF)
+		{
+			System.out.println(t.getType().toString() + "\t:" + t.getData());
+			writeToken(t);
+			t = s.getNextToken();
+		}
+	}
+	
+	public static void writeToken(Token t)
+	{
+		
+		try {
+			outFile.write(t.getType() + "\t:" + t.getData());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
