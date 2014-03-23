a1e4d09f5cdaf388aa58bc911aeaded08309db49
yahooguntu <yahooguntu@imgur.com>
2014-03-22 23:56:22 -0400

    Initial stuff.

---
+++ src/parser/Program.java
@@ -0,0 +1,14 @@
+package parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program
+{
+	private List<Declaration> declarations;
+	
+	public Program(List<Declaration> decls)
+	{
+		declarations = decls;
+	}
+}
