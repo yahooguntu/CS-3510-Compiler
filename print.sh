05e727303537d22bacdd0b0945183f33d72608e5
yahooguntu <yahooguntu@hit-nxdomain.opendns.com>
2014-03-27 21:39:20 -0400

    Modified print program.

--- print.sh
+++ print.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 a2ps -R -s1 --media=letter --columns=1 --rows=1 --chars-per-line=80 --major=rows --border=no --tabsize=3 src/parser/* src/parser/expression/* src/parser/statement/* tests/* print.sh -o - | ps2pdf - Program.pdf
 pdfunite write-ups/Project2WriteUp.pdf Grammar-MkII.pdf First-Follow-MkII.pdf Program.pdf Printout.pdf
+rm Program.pdf
